859785779b1e73d3eab9f0df8fc4a5769e163848
Victor Mougel <victor.mougel.etu@univ-lille.fr>
2021-11-21 15:57:43 +0100

    adding new projects and cool effects

---
+++ script.js
@@ -0,0 +1,51 @@
+function darkMode(){
+    var bd=document.getElementById('bd')
+    var cards=[document.getElementById("firstCard"),document.getElementById("secondCard"),document.getElementById("thirdCard"),document.getElementById("fourthCard")]
+    var triggerButton=document.getElementById('btn')
+    var param=window.getComputedStyle(document.body, null).getPropertyValue('background-color')
+
+    if(param=="rgb(255, 255, 255)"){
+        bd.style.backgroundColor="#181A1B";
+        bd.style.color="white";
+        triggerButton.className="btn btn-dark"
+        cards.forEach(element => {
+            element.className="card text-white bg-dark"
+        });
+        sessionStorage.setItem("theme", "dark")
+
+    }
+    else{
+        bd.style.backgroundColor="white"
+        bd.style.color="black"
+        triggerButton.className="btn btn-light"
+        cards.forEach(element => {
+            element.className="card"
+        });
+        sessionStorage.setItem("theme", "light")
+    }
+
+};
+
+function restore(){
+    var cards=[document.getElementById("firstCard"),document.getElementById("secondCard"),document.getElementById("thirdCard"),document.getElementById("fourthCard")]
+    var bd=document.getElementById('bd')
+    var theme=sessionStorage.getItem("theme")
+    var triggerButton=document.getElementById('btn')
+
+    if(theme=="dark"){
+        bd.style.backgroundColor="#181A1B"
+        bd.style.color="white"
+        triggerButton.className="btn btn-dark"
+        cards.forEach(element => {
+            element.className="card text-white bg-dark"
+        });
+
+    }else{
+        bd.style.backgroundColor="white"
+        bd.style.color="black"
+        triggerButton.className="btn btn-light"
+        cards.forEach(element => {
+            element.className="card"
+        });
+    }
+}
