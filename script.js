eeb7755f64749ed578dcc64e290ef74fb5c03c38
Victor Mougel <victor.mougel.etu@univ-lille.fr>
2022-01-20 21:22:32 +0100

    Ajusted size of blender images to make them bigger and added a blender project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+//This is probably not the best way to do it
 function darkMode(){
     var bd=document.getElementById('bd')
-    var cards=[document.getElementById("firstCard"),document.getElementById("secondCard"),document.getElementById("thirdCard"),document.getElementById("fourthCard")]
+    var cards=[document.getElementById("SteinsCard"),document.getElementById("firstCard"),document.getElementById("secondCard"),document.getElementById("thirdCard"),document.getElementById("fourthCard")]
     var triggerButton=document.getElementById('btn')
     var param=window.getComputedStyle(document.body, null).getPropertyValue('background-color')
 
@@ -27,7 +28,7 @@ function darkMode(){
 };
 
 function restore(){
-    var cards=[document.getElementById("firstCard"),document.getElementById("secondCard"),document.getElementById("thirdCard"),document.getElementById("fourthCard")]
+    var cards=[document.getElementById("SteinsCard"),document.getElementById("firstCard"),document.getElementById("secondCard"),document.getElementById("thirdCard"),document.getElementById("fourthCard")]
     var bd=document.getElementById('bd')
     var theme=sessionStorage.getItem("theme")
     var triggerButton=document.getElementById('btn')
