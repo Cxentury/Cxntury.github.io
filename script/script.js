3b2a3e03b0e1c14e0cdfb4cef61d7dc155a493f0
Cxntury <victormougel.etu@gmail.com>
2022-11-26 12:57:24 +0100

    fixed js for navbar and deleted images

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
-document.getElementById("hiddenNav").addEventListener("click",function(event) {
-    document.getElementById("navigation").style.display="block";
+let navigation = document.getElementById("navigation");
+let dotsNav = document.getElementById("hiddenNav");
+
+dotsNav.addEventListener("click", function (event) {
+  navigation.style.display = "block";
 })
 
-window.addEventListener('click', function(e){   
-    if (document.getElementById('navigation').contains(e.target)){
-      // Clicked in box
-    } else if(!document.getElementById('hiddenNav').contains(e.target)){
-        document.getElementById("navigation").style.display="none";
-    }
-  });
+window.addEventListener('click', function (e) {
+  if (window.getComputedStyle(dotsNav, null).display === "block" && !document.getElementById('hiddenNav').contains(e.target)) {
+    navigation.style.display = "none";
+  }
+});
