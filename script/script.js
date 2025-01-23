fa85b921e611e7890a1eecadccbc8c7585d952bb
Cxentury <victormougel.etu@gmail.com>
2025-01-24 00:29:32 +0100

    Update Blender

--- script/script.js
+++ script/script.js
@@ -1,15 +1,16 @@
 let navigation = document.getElementById("navigation");
-let dotsNav = document.getElementById("hiddenNav");
+// let dotsNav = document.getElementById("hiddenNav");
 
-dotsNav.addEventListener("click", function (event) {
-  navigation.style.display = "block";
-})
+// dotsNav.addEventListener("click", function (event) {
+//   navigation.style.display = "block";
+// })
 
-window.addEventListener('click', function (e) {
-  if (window.getComputedStyle(dotsNav, null).display === "block" && !document.getElementById('hiddenNav').contains(e.target)) {
-    navigation.style.display = "none";
-  }
-});
+// window.addEventListener('click', function (e) {
+//   if (window.getComputedStyle(dotsNav, null).display === "block" && !document.getElementById('hiddenNav').contains(e.target)) {
+//     navigation.style.display = "none";
+//   }
+// });
+
 
 function myFunction() {
   var x = document.getElementById("navMenu");
